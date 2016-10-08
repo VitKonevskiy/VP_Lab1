80a03654bcefbbd1d68f24d838d199b5c0e23b91
Vitaliy Konevskiy <elisey_kvo@mail.ru>
2016-10-08 22:48:39 +0300

    Finished

---
+++ Dohod_po_vkladu/MyForm.cpp
@@ -0,0 +1,15 @@
+#include "MyForm.h"
+
+using namespace System;
+using namespace System::Windows::Forms;
+
+
+[STAThread]
+void Main(array<String^>^ args)
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Dohod_po_vkladu::MyForm form;
+	Application::Run(%form);
+}
